5e14b6f79a008154c0a0c63c21220c46c24b8b95
ikumen <ikumen@gnoht.com>
2018-08-28 23:34:53 -0700

    Example of utility/service class. Added missing babel config

--- frontend/containers/TodoService/index.js
+++ frontend/containers/TodoService/index.js
@@ -1,25 +1,11 @@
-import React from 'react';
 
-class TodoService extends React.Component {
-   constructor(props) {
-      super(props);
-      this.state = {}
-   }
+class TodoService {
 
-   async getTodoList() {
+    async getTodoList() {
       let resp = await fetch('/api/todos');
       return await resp.json();
   }
 
-  componentDidMount() {
-      this.getTodoList()
-         .then(items => {this.setState({items: items})})
-         .catch(err => {
-            console.log(err)
-         });
-  }
-
-   render() {
-      return <>{this.props.children}</>
-   }
 }
+
+export default new TodoService();
